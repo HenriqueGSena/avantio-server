c2b40475dfd4f70117e42e1df5c0da28d1f0933e
henrique.sena <carlos.sena@pineapples.com.br>
2024-10-28 18:05:08 -0300

    feature/configprisma
    * Ajustando estrutura do controller da aplicacao;

--- src/controller/bookingController.ts
+++ src/controller/bookingController.ts
@@ -1,5 +1,5 @@
 import { Controller, Get, Put } from '@nestjs/common';
-import api from '../../api/avantioApi';
+import api from '../api/avantioApi';
 
 @Controller()
 export class BookingController {}

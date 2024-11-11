ef70f5d6fdb1f2b6df19bdbc350cb4833aa392b5
henrique.sena <carlos.sena@pineapples.com.br>
2024-11-11 17:42:26 -0300

    feature/configprisma
    * Adicionando mais um status na busca pelos ids de reserva;
    * Alterando nome da pasta para enum;

--- src/avantio/bookings/bookings.service.ts
+++ src/avantio/bookings/bookings.service.ts
@@ -1,22 +1,26 @@
 import { Injectable, Inject } from '@nestjs/common';
-import { BookingStatus } from '../types/bookingStatus';
+import { BookingStatus } from '../enum/bookingStatus';
 
 @Injectable()
 export class BookingsService {
 
     constructor(@Inject('API_SERVICE') private readonly apiService) { }
 
-    async getConfirmedBookings(): Promise<any> {
+    async getConfirmedBookings(): Promise<string[]> {
         try {
             const response = await this.apiService.get('/bookings');
             const bookings = response.data.data;
 
-            const confirmedBookings = bookings.filter(
-                (booking) => booking.status === BookingStatus.CONFIRMED || booking.status === BookingStatus.UNPAID,
+            const filteredBookings = bookings.filter(
+                (booking) =>
+                    booking.status === BookingStatus.CONFIRMED ||
+                    booking.status === BookingStatus.UNPAID ||
+                    booking.status === BookingStatus.PAID,
             );
-            return confirmedBookings;
+            const bookingIds = filteredBookings.map((booking) => booking.id);
+            return bookingIds;
         } catch (er) {
-            console.error('Erro ao retorna lista de reservas', er);
+            console.error('Erro ao retorna lista de ids das reservas', er);
         }
     }
 }

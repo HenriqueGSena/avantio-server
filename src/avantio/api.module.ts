e517d0a970e8ab48a00aa5b442587d18eed75693
henrique.sena <carlos.sena@pineapples.com.br>
2024-11-12 17:51:38 -0300

    feature/configprisma
    * Criando funcao para estar pegando os dados da reserva com seus detalhes;
    * Instalando dependencia @nestjs/schedule e implementando cron para busca por lista;
    * Ajuste de parametros na requisicao de lista bookings;

--- src/avantio/api.module.ts
+++ src/avantio/api.module.ts
@@ -1,10 +1,12 @@
 import { Module } from '@nestjs/common';
+import { ScheduleModule } from '@nestjs/schedule';
 import { ConfigServiceApi } from './config.server';
 import axios from 'axios';
 import { BookingsController } from './bookings/bookings.controller';
 import { BookingsService } from './bookings/bookings.service';
 
 @Module({
+  imports: [ScheduleModule.forRoot()],
   controllers: [BookingsController],
   providers: [
     ConfigServiceApi,
--- src/avantio/bookings/bookings.service.ts
+++ src/avantio/bookings/bookings.service.ts
@@ -1,25 +1,42 @@
 import { Injectable, Inject, OnModuleInit } from '@nestjs/common';
+import { Cron } from '@nestjs/schedule';
 import { BookingStatus } from '../enums/bookingStatus';
 
 @Injectable()
 export class BookingsService implements OnModuleInit {
+    private confirmedBookingIds: { id: string }[] = [];
     constructor(@Inject('API_SERVICE') private readonly apiService) { }
 
     async onModuleInit() {
-        const bookingIds = await this.getConfirmedBookings();
-        console.log('Ids das reservas confirmadas:', bookingIds);
+        this.confirmedBookingIds = await this.getConfirmedBookings();
+        // console.log('Ids das reservas confirmadas:', this.confirmedBookingIds);
+
+        const bookingDetails = await this.getBookingsDetailsId();
+        // console.log('Detalhes das reservas confirmadas:', bookingDetails);
     }
 
-    async getConfirmedBookings(): Promise<{ id: string; departure: string }[]> {
+    @Cron('0 */15 * * * *')
+    async checkBookings() {
+        this.confirmedBookingIds = await this.getConfirmedBookings();
+    }
+
+    async getConfirmedBookings(): Promise<{ id: string }[]> {
         try {
             const today = new Date().toISOString().split('T')[0];
+            const threeDaysAgo = new Date;
+            threeDaysAgo.setDate(threeDaysAgo.getDate() - 3);
+            const threeDaysAgoFormatted = threeDaysAgo.toISOString().split('T')[0];
+
+            console.log('Data atual:', today);
+            console.log('Data de três dias atrás:', threeDaysAgoFormatted);
 
             const response = await this.apiService.get('/bookings', {
                 params: {
-                    departureDate_from: today,
+                    departureDate_from: threeDaysAgoFormatted,
                     departureDate_to: today,
                 },
             });
+            
             const bookings = response.data.data;
 
             const filteredBookings = bookings
@@ -34,14 +51,37 @@ export class BookingsService implements OnModuleInit {
                 })
                 .map((booking) => ({
                     id: booking.id,
-                    // departure: booking.stayDates.departure,
                 }));
 
             return filteredBookings;
-        } catch (error) {
-            console.error('Erro ao retornar lista de ids das reservas', error);
+        } catch (e) {
+            console.error('Erro ao retornar lista de ids das reservas', e);
             return [];
         }
     }
+
+    async getBookingsDetailsId(): Promise<{ id: string; departure: string; statusService: string | null }[]> {
+        try {
+            const detailsBooking = await Promise.all(
+                this.confirmedBookingIds.map(async (booking) => {
+                    const response = await this.apiService.get(`/bookings/${booking.id}`);
+                    const bookingData = response.data.data;
+
+                    return {
+                        id: bookingData.id,
+                        departure: bookingData.stayDates.departure,
+                        statusService: bookingData.extras.length > 0 ? bookingData.extras[0].info.category.code : null,
+                    };
+                })
+            );
+
+            return detailsBooking;
+
+        } catch (error) {
+            console.error('Erro ao buscar detalhes das reservas por ID:', error);
+            return [];
+        }
+    }
+
 }
 

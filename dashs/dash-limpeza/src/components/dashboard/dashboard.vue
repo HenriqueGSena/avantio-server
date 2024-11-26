f74b734289a2ffa5cb2cfaa9fdcadfebaf008f62
henrique.sena <carlos.sena@pineapples.com.br>
2024-11-26 09:19:13 -0300

    Ajuste de dashboard no painel;

--- dashs/dash-limpeza/src/components/dashboard/dashboard.vue
+++ dashs/dash-limpeza/src/components/dashboard/dashboard.vue
@@ -1,13 +1,13 @@
 <template>
     <div class="px-4 sm:px-6 lg:px-8 overflow-hidden">
-        <div class="bg-[#FCC117] p-6 rounded-lg">
+        <div class="bg-[#FCC117] rounded-lg">
             <div class="sm:flex sm:items-center sm:justify-between">
                 <div class="sm:flex sm:items-center sm:space-x-5">
                     <div class="shrink-0">
                         <img class="mx-auto size-28 rounded-full" src="../../assets/pine.png" alt="">
                     </div>
                     <div class="flex flex-col items-center sm:items-start">
-                        <span class="text-xl font-bold text-gray-900 sm:text-5xl"
+                        <span class="text-xl font-bold text-gray-900 sm:text-4xl"
                             v-text="$t('lang.dash.cleaning')">
                         </span>
                     </div>

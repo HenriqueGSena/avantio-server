32b9737e7ac8e5f41cc288391a21a28d548c2719
henrique.sena <carlos.sena@pineapples.com.br>
2024-12-12 18:57:27 -0300

    Alterando funcao para pegar o id da lista de threads;

--- pine-serve/src/avantio/messaging/messagingServer.ts
+++ pine-serve/src/avantio/messaging/messagingServer.ts
@@ -14,24 +14,45 @@ export class MessagingService implements OnModuleInit {
         this.apiService = createAxiosClient(this.configService);
     }
 
-    async onModuleInit() {
+    public async onModuleInit() {
         this.listIdsMessaging = await this.getListThreadsMessages();
+        console.log("List of IDs:", this.listIdsMessaging);
     }
 
     public async getListThreadsMessages(): Promise<{ id: string }[]> {
+
+        // todo: resolver erro 504 na requisição feita para criar a lista de id threads;
+
         try {
+            const startOfYear = new Date(new Date().getFullYear(), 0, 1).toISOString().split('T')[0];
+            const today = new Date().toISOString().split('T')[0];
+
+            console.log('Data do início do ano:', startOfYear);
+            console.log('Segue a data atual:', today);
+
             let url: string | null = '/threads';
-            const paginationSize = 100;
+            let firstRequest = true;
+            const paginationSize = Math.min(30, 100);
+            this.listIdsMessaging = [];
+            console.log('Inicializando a lista vazia', this.listIdsMessaging);
+            while (url) {
+                const response = await this.apiService.get(url, {
+                    params: firstRequest ? {
+                        pagination_size: paginationSize,
+                        booking_creationDate_from: startOfYear,
+                        booking_creationDate_to: today,
+                    } : {},
+                    timeout: 60000,
+                });
+                const data = response.data;
+                console.log('Data retornada:', data);
 
-            const response = await this.apiService.get(url, {
-                params: {
-                    pagination_size: paginationSize,
-                },
-            });
+                this.listIdsMessaging = this.listIdsMessaging.concat(data.data);
 
-            const data = response.data;
-            this.listIdsMessaging = this.listIdsMessaging.concat(data.data);
-
+                url = data._links?.next || null;
+                firstRequest = false;
+            }
+            console.log('Finalizando com os IDs retornados:', this.listIdsMessaging);
             return this.listIdsMessaging
         } catch (e) {
             console.error('Error ao retornar lista de ids messaging', e);
